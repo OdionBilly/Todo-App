882d3e906d0b21753649cc17fceb36ed91c17fee
Odion <odionfiona@gmail.com>
2024-10-09 14:39:17 +0300

    added a background image 	modified:   src/App.jsx 	modified:   src/component/Addform.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import MoonIcon from "./assets/icon-moon.svg";
 import SunIcon from "./assets/icon-sun.svg";
+import Background from "./assets/bg-desktop-light.jpg"
 import './App.css'
 import Addform from "./component/Addform";
 import Nav from "./component/Nav";
@@ -64,7 +65,7 @@ function App() {
   return ( 
     
       <div className={`${darkTheme ? "bg-slate-900"  : " bg-[#f2f2f2]" } min-h-screen pb-20 relative`} >
-        <div className="bg-light_header_image_mobile lg:bg-light_header_image bg-no-repeat bg-cover h-52 px-5 lg:px-0 pt-10">
+        <div className="bg-light_header_image_mobile lg:bg-light_header_image bg-no-repeat bg-cover h-52 px-5 lg:px-0 pt-10" style={{backgroundImage : `url(${Background})`}}>
           <div className="flex justify-between lg:w-3/6 lg:mx-auto">
             <p className="Text-3xl font-semibold text-white tracking-[6px]">TODO</p>
 
